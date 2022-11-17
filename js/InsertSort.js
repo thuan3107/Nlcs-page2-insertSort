4da336eb8b751d4518ed2a741941a92e0f4f3012
Huu Thuan <huuthuanlt336@gmail.com>
2022-11-17 22:47:41 +0700

    insertSort ver3

--- js/InsertSort.js
+++ js/InsertSort.js
@@ -5,9 +5,9 @@ let countSwap = document.querySelector('.countSwap');
 let condition = document.querySelector('.condition');
 
 function setup() {
-  createCanvas(windowWidth - 180, windowHeight - 50);
+  createCanvas(windowWidth - 200, windowHeight - 50);
   frameRate(15);
-  Al = new Sort(200, 50, windowWidth * 0.75, windowHeight * 0.5);
+  Al = new Sort(200, 50, windowWidth * 0.7, windowHeight * 0.5);
   Al.shuffle();
   condition.classList.toggle('active');
   countSwap.classList.toggle('active');
